6928b090d6664520932dd683e4acce78a470d262
juliawqy <149480793+juliawqy@users.noreply.github.com>
2024-05-25 13:37:57 +0800

    node setup

--- proxy.js
+++ proxy.js
@@ -16,3 +16,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.sendFile("index.html", {root: "./public"});
 })
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('listening on port 3000;');
+});
