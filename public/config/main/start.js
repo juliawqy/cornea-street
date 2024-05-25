7e7a834de6fc461f80df458dd7289e44686575be
juliawqy <149480793+juliawqy@users.noreply.github.com>
2024-05-25 13:19:06 +0800

    page init

---
+++ public/config/main/start.js
@@ -0,0 +1,141 @@
+const key = "AIzaSyAmU2Q8AKdlcwSQ5VF7_TtZo1AVnlX4GsE"
+
+let map, directionsService, directionsRenderer, sourceAutoComplete, destAutoComplete;
+
+const app = Vue.createApp({
+    data() {
+        return {
+            src : "",
+            input: "",
+            currLat: 0,
+            currLng: 0,
+        }
+    },
+
+    methods: {
+
+        initMap() {
+
+            console.log("running initMap")
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: this.currLat, lng: this.currLng},
+                zoom: 20
+            });
+            google.maps.event.addListener(map, 'click', function(event) {
+                this.setOptions({scrollwheel:true});
+
+            addMarker({coords:event.latLng});
+            });
+            directionsService = new google.maps.DirectionsService();
+            directionsRenderer = new google.maps.DirectionsRenderer();
+            directionsRenderer.setMap(map);
+
+            sourceAutoComplete = new google.maps.places.Autocomplete(document.getElementById('start'));
+            destAutoComplete = new google.maps.places.Autocomplete(document.getElementById('dest'));
+        },
+
+        calcRoute() {
+
+            document.getElementById('transitNav').className = "input-group mb-2 p-0 d-flex"
+
+            var source = document.getElementById('start').value;
+            var dest = document.getElementById('dest').value;
+
+            let request = {
+                origin: source,
+                destination: dest,
+                travelMode: 'DRIVING'
+            };
+
+            directionsService.route(request, function(result, status) {
+                if(status == 'OK') {
+                    data = result;
+                    console.log(data)
+
+                    directionsRenderer.setDirections(data);
+                }
+                else {
+                    console.log(result)
+                }
+            });
+
+        },
+
+        getLocation() {
+            if (navigator.geolocation) {
+                console.log("getting location")
+                navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
+            } else {
+                console.log("Geolocation is not supported by this browser.");
+            }
+        },
+        
+        showPosition(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            console.log("setting location")
+            this.currLat = latitude;
+            this.currLng = longitude;
+
+            this.initMap();
+            
+            console.log("Latitude: " + latitude);
+            console.log("Longitude: " + longitude);
+
+            // this.src = `https://www.google.com/maps/embed/v1/view?key=${key}&center=${latitude},${longitude}&zoom=30`;
+            // this.src = latitude + "," + longitude
+
+
+            // console.log(this.src)
+            // return (latitude, longitude)
+        },
+        
+        showError(error) {
+            let errorMsg = '';
+        
+            switch(error.code) {
+                case error.PERMISSION_DENIED:
+                    errorMsg = "User denied the request for Geolocation.";
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    errorMsg = "Location information is unavailable.";
+                    break;
+                case error.TIMEOUT:
+                    errorMsg = "The request to get user location timed out.";
+                    break;
+                case error.UNKNOWN_ERROR:
+                    errorMsg = "An unknown error occurred.";
+                    break;
+            }
+
+            console.log(errorMsg);
+        },
+
+        search() {
+            let inputString = this.input;
+            let splitStr = inputString.split(" ")
+            let query = ""
+
+            for (str of splitStr) {
+                query += str + "+"
+            }
+
+            query = query.substring(0, query.length - 1)
+
+            this.src = `https://www.google.com/maps/embed/v1/place?key=${key}&q=${query}&zoom=14`;
+
+
+            console.log(this.src)
+        }
+    },
+
+    created() {
+        this.getLocation();
+    }
+
+}).mount('#app')
+
+
+
